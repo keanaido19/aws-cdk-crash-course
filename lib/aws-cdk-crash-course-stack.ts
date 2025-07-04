73b41503e808ff033b15247468b45032f7c8febb
Keaton Lawrence Naidoo <keanaido19@gmail.com>
2025-07-04 11:46:09 +0200

    Setup an SQS Queue and added a push notification to the queue when an object is created in an S3 bucket.

--- lib/aws-cdk-crash-course-stack.ts
+++ lib/aws-cdk-crash-course-stack.ts
@@ -1,6 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
-import {Bucket, CfnBucket} from 'aws-cdk-lib/aws-s3';
-import { Construct } from 'constructs';
+import {Bucket, CfnBucket, EventType} from 'aws-cdk-lib/aws-s3';
+import {Construct} from 'constructs';
+import {Queue} from "aws-cdk-lib/aws-sqs";
+import {SqsDestination} from "aws-cdk-lib/aws-s3-notifications";
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCdkCrashCourseStack extends cdk.Stack {
@@ -15,5 +18,12 @@ export class AwsCdkCrashCourseStack extends cdk.Stack {
     const level2S3Bucket = new Bucket(this, 'MyFirstLevel2ConstructBucket',
         { bucketName: "myfirstlevel2constructbucketkeanaido19", versioned: true }
     );
+
+    const queue = new Queue(this, 'MyQueue',
+        { queueName: 'MyQueue' }
+    );
+
+    level2S3Bucket.addEventNotification(EventType.OBJECT_CREATED, new SqsDestination(queue));
+
   }
 }
